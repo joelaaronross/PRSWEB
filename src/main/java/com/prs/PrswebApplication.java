6e63b9073f5a8d06047044244163782c0ca0a036
unknown <joelaaronross>
2018-09-28 16:19:35 -0700

    spring prs proj

---
+++ src/main/java/com/prs/PrswebApplication.java
@@ -0,0 +1,12 @@
+package com.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrswebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/prs/business/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.prs.business.user;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	
+	
+}
